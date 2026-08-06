34361d46ad6a870d19ab6779e4f5234236ea6df3
padelarenareggioemilia <padelarenareggioemilia@gmail.com>
2026-08-06 18:57:16 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,13 @@
-const CACHE='pam-v9-9-captain-complete-9903';
-const STATIC=['./','./start.html','./login.html','./account-home.html','./captain-home.html','./app-theme.css','./app-ui.js','./assets/aics-logo.png'];
-
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)).catch(()=>{}));
-  self.skipWaiting();
+const CACHE='pam-v9-9-4-captain-full';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>{
+ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));
+ self.clients.claim();
 });
-
-self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET') return;
-  const url=new URL(event.request.url);
-
-  if(event.request.mode==='navigate' || url.pathname.endsWith('.html')){
-    event.respondWith(
-      fetch(event.request,{cache:'no-store'})
-        .then(response=>{
-          const copy=response.clone();
-          caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-          return response;
-        })
-        .catch(()=>caches.match(event.request).then(found=>found||caches.match('./start.html')))
-    );
-    return;
-  }
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response=>{
-        const copy=response.clone();
-        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-        return response;
-      })
-      .catch(()=>caches.match(event.request))
-  );
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET')return;
+ const u=new URL(e.request.url);
+ if(e.request.mode==='navigate'||u.pathname.endsWith('.html')){
+  e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));
+ }
 });
